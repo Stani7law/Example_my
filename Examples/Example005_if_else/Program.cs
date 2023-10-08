918c6ee64471d91d8e7f907a928687a7256dfd07
Stani7law <staskriwitsky27@gmail.com>
2023-10-08 12:22:46 +0300

    Программа по выводу имен

---
+++ Examples/Example005_if_else/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+ string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша");
+    Console.WriteLine (username);
+}
+else
+{
+    Console.Write ("Привет, ");
+    Console.WriteLine (username);
+}
